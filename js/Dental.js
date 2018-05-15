70c42caf2b33f83057b8c4cb1579929ad869dd58
David Kaliner <davidkaliner@gmail.com>
2018-05-15 05:26:46 +0300

    structure

--- js/Dental.js
+++ js/Dental.js
@@ -13,11 +13,13 @@ var mainFunction = (function()
       };
       date_input.datepicker(options);
     })*/
-    
-    
+    var url = 'http://ec2-18-221-134-60.us-east-2.compute.amazonaws.com';
+    var CLIENT_ID = '512318917553-vukvellsr2371gfrcj197ms2hc1g2hni.apps.googleusercontent.com';
+    var API_KEY = '<YOUR_API_KEY>';
+    var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+    var SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
     
     function userLogin(){
-        var url = 'http://8e989ab7.ngrok.io';
         var dataLogin = {
               Username: document.getElementById('InputUsername').value,
               Password: document.getElementById('InputPassword').value
@@ -33,15 +35,16 @@ var mainFunction = (function()
                 response = response.replace(/\"/g, "");
                 if(response == "Found doctor")
                 {
-                   $("#login-page").hide();
+                    $("#login-page").hide();
                     $("#main-page").hide();
                     $("#google-calender-page").show();
+                     
                 }
                 else(response == "Found patient")
                 {
                    $("#login-page").hide();
                     $("#main-page").show();
-                    
+                     
                 }
             },
             error: function(xhr) {
@@ -51,7 +54,6 @@ var mainFunction = (function()
     }
     
     function registerUser(){
-        var url = 'http://8e989ab7.ngrok.io';
         var dataTest = {
             Name: "test doctor",
             Username: "testdoc",
@@ -73,10 +75,90 @@ var mainFunction = (function()
         });
     }
     
+    function ScheduleAppointment(){
+        
+        
+    }
+    
+    function handleClientLoad() {
+        gapi.load('client:auth2', initClient);
+      }
+    
+    function initClient() {
+        gapi.client.init({
+          apiKey: API_KEY,
+          clientId: CLIENT_ID,
+          discoveryDocs: DISCOVERY_DOCS,
+          scope: SCOPES
+        }).then(function () {
+          // Listen for sign-in state changes.
+          gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+          // Handle the initial sign-in state.
+          updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+          authorizeButton.onclick = handleAuthClick;
+          signoutButton.onclick = handleSignoutClick;
+        });
+      }
+    
+    function updateSigninStatus(isSignedIn){
+        if (isSignedIn) {
+          authorizeButton.style.display = 'none';
+          signoutButton.style.display = 'block';
+          listUpcomingEvents();
+        } else {
+          authorizeButton.style.display = 'block';
+          signoutButton.style.display = 'none';
+        }
+      }
+    
+    function handleAuthClick(event) {
+        gapi.auth2.getAuthInstance().signIn();
+      }
+    
+    function handleSignoutClick(event) {
+        gapi.auth2.getAuthInstance().signOut();
+      }
+    
+    function appendPre(message) {
+        var pre = document.getElementById('content');
+        var textContent = document.createTextNode(message + '\n');
+        pre.appendChild(textContent);
+      }
+    
+    function listUpcomingEvents() {
+        gapi.client.calendar.events.list({
+          'calendarId': 'primary',
+          'timeMin': (new Date()).toISOString(),
+          'showDeleted': false,
+          'singleEvents': true,
+          'maxResults': 10,
+          'orderBy': 'startTime'
+        }).then(function(response) {
+          var events = response.result.items;
+          appendPre('Upcoming events:');
+
+          if (events.length > 0) {
+            for (i = 0; i < events.length; i++) {
+              var event = events[i];
+              var when = event.start.dateTime;
+              if (!when) {
+                when = event.start.date;
+              }
+              appendPre(event.summary + ' (' + when + ')')
+            }
+          } else {
+            appendPre('No upcoming events found.');
+          }
+        });
+      }
+    
     $(document).ready(function(){
         $("#login-page").show();
-        $("#main-page").hide();
         $("#calender-page").hide();
+        $("#PatientFilesButton").hide();
+        $("#main-page").hide();
+        $("#update-info").hide();
          
         $(document.body).on('click',"#SubmitLogin",function () {
             userLogin();
@@ -89,7 +171,43 @@ var mainFunction = (function()
         $(document.body).on('click',"#AppointmentButton",function () {
             $("#login-page").hide();
             $("#main-page").hide();
+            $("#PatientFilesButton").hide();
             $("#calender-page").show();
+            $("#update-info").hide();
+            
+        });
+        
+        $(document.body).on('click',"#PatientFilesButton",function () {
+            $("#login-page").hide();
+            $("#main-page").hide();
+            $("#calender-page").hide();
+            $("#patient-files").show();
+            $("#update-info").hide();
+        });
+        
+        $(document.body).on('click',"#BackDatePick",function () {
+            $("#login-page").hide();
+            $("#calender-page").hide();
+            $("#patient-files").hide();
+            $("#main-page").show();
+            $("#update-info").hide();
+        });
+        
+        
+        $(document.body).on('click',"#UpdateInfo",function () {
+            $("#login-page").hide();
+            $("#main-page").hide();
+            $("#calender-page").hide();
+            $("#PatientFilesButton").hide();
+            $("#update-info").show();
+        });
+        
+         $(document.body).on('click',"#BackUpdateInfo",function () {
+            $("#login-page").hide();
+            $("#calender-page").hide();
+            $("#patient-files").hide();
+            $("#main-page").show();
+            $("#update-info").hide();
         });
         
         $('#datetimepicker12').datetimepicker({
